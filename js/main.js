b8ac4ad8c6c711da509610d5fa9edf9c3a921981
Alexander Nsowah <alexanderglover478@gmail.com>
2023-04-10 22:07:48 +0000

    adding-client

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
 // alert('abenaa')
+// https://dark-puce-turkey-tutu.cyclic.app
+
+
+
+  const btn = document.querySelector("#btn-sumbit");
+     btn.addEventListener('click', rapperApi)
+
+       async function rapperApi() {
+        const value = document.querySelector('#name').value;
+        try{
+         const response = await fetch(`https://dark-puce-turkey-tutu.cyclic.app/api/${value}`)
+          const data = await response.json()
+
+          console.log(data);
+          document.querySelector('h2').innerText = data.stageName;
+          document.querySelector('h3').innerText = data.birthName;
+          document.querySelector('img').src = data.img;
+        }
+      catch(error){
+         console.log('error at' + error)
+      }  
+      
+
+ 
+}
